ad8482fc5785663d8b080a519f434ba1252e56a5
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-01-20 16:32:32 +0800

    Update keep.js

--- qx/keep.js
+++ qx/keep.js
@@ -8,16 +8,8 @@ if ($response.body!==undefined){
     body=body.replace(/\"status\":\w+/g,`"status":true`)
     body=body.replace(/\"errorCode\":\d+/g,`"errorCode":0`)
     body=body.replace(/\"videoTime\":\d+/g,`"videoTime":3000`)
-    var resp=JSON.parse(body)
-    if (resp["data"]!==undefined && resp["data"]["permissions"]!==undefined){
-        let vip=resp["data"]["permissions"]
-
-        for (let key in vip){
-            vip[key]["status"]=true
-        }
-        $response.body=JSON.stringify(resp)
-        $done($response)
-    }
+    $done(body)
+    
 }else{
-    $done($response)
+    $done()
 }
