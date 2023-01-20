a8ffddee6f23960ba6b9bf45480e37b8dafa1813
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-01-20 16:27:24 +0800

    Update keep.js

--- qx/keep.js
+++ qx/keep.js
@@ -1,23 +1,23 @@
 if ($response.body!==undefined){
-    
-    var resp=JSON.parse($response.body)
+    body=$response.body
+
+    body=body.replace(/\s*/g,"")
+    body =body.replace(/\"username\":".*?"/g,`"username":"枉久解锁"`)
+    body=body.replace(/\"memberStatus\":\d+/g,`"memberStatus":1`)
+    body=body.replace(/\"buttonText\:.*?"/,"已永久")
+    body=body.replace(/\"status\":\w+/g,`"status":true`)
+    body=body.replace(/\"errorCode\":\d+/g,`"errorCode":0`)
+    body=body.replace(/\"videoTime\":\d+/g,`"videoTime":3000`)
+    var resp=JSON.parse(body)
     if (resp["data"]!==undefined && resp["data"]["permissions"]!==undefined){
         let vip=resp["data"]["permissions"]
-    resp["data"]["username"]="枉久解锁"
-    for (let key in vip){
-        vip[key]["status"]=true
+
+        for (let key in vip){
+            vip[key]["status"]=true
+        }
+        $response.body=JSON.stringify(resp)
+        $done($response)
     }
-        $done({body:JSON.stringify(resp)})
-        
-    }else{
-        $done($response.body)    
-    }
-    
 }else{
-$done()
+    $done($response)
 }
-    
-
-
-
-
