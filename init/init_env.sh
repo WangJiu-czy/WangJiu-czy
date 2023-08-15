00fed7f7a4160c23e90d48e0b22da76950c3b2e3
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-08-15 14:01:32 +0800

    Create init_env.sh

---
+++ init/init_env.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+pd="hadoop"
+proxy="https://ghproxy.com/"
+hostname="master"
+
+
+echo "===================更新软件包,安装依赖========================="
+if command -v yum &>/dev/null;then
+        echo $pd |sudo -S yum install -y net-tools wget curl vim
+        rpm -e `rpm -qa |grep openjdk`
+else
+        echo $pd |sudo -S apt install net-tools wget curl vim
+         sudo apt-get remove openjdk*
+fi
+echo "==================免密设置==========================="
+if [ -d ~/.ssh ]; then
+     echo "已经设置过ssh免密"
+
+else
+  #TODO:使用超时判断有点bug
+    ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa && cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys && chmod 0600 ~/.ssh/authorized_keys
+    echo "ssh 设置成功!"
+fi
+
+echo "===================配置ip映射================================"
+
+ip=`ifconfig ens33|grep "inet " |awk '{ print $2}'`
+echo $pd |sudo -S hostnamectl set-hostname "$hostname"
+echo $pd | sudo -S sh -c "echo '${ip} $hostname' >> /etc/hosts"
+echo $pd | sudo -S sh -c "sed -i 's/[0-9\.]\+[ ]\+$hostname/$ip $hostname/g' /etc/hosts"
+
+
+echo "=====================创建目录============================="
+echo $pd |sudo -S mkdir -p /opt/install
+echo $pd |sudo -S chown -R $USER:$USER /opt/install
