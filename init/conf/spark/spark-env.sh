4af1d305f31227f7bd54c7ab5107c8904fcafb00
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-01 11:48:06 +0800

    Update spark-env.sh

--- init/conf/spark/spark-env.sh
+++ init/conf/spark/spark-env.sh
@@ -7,6 +7,7 @@ export SPARK_WORKER_MEMOBY=1g
 export SPARK_WORKER_PORT=7078
 export SPARK_WORKER_WEBUI_PORT=7080
 
+export HADOOP_CONF_DIR=/opt/install/hadoop/etc/hadoop
 #配置指定的python解释器,这个是使用anaconda创建的虚拟环境
 export PYSPARK_PYTHON=/opt/install/anaconda3/bin/python3
 
