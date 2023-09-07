3dd0f2d4b7b0fce06bb6f8bc21b107754df99358
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-07 22:50:29 +0800

    Update install-zk.sh

--- init/install-zk.sh
+++ init/install-zk.sh
@@ -1,4 +1,4 @@
 
 mkdir -p /opt/install/zookeeper/data && echo "1" > /opt/install/zookeeper/data/myid
-mv /opt/install/zookeeper/zoo-sample.cfg  /opt/install/zookeeper/zoo.cfg
+mv /opt/install/zookeeper/conf/zoo_sample.cfg  /opt/install/zookeeper/conf/zoo.cfg
 sed -i 's,/tmp/zookeeper,/opt/install/zookeeper/data,g' /opt/install/zookeeper/conf/zoo.cfg
