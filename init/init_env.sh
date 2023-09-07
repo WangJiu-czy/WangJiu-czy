55fb61c624093654f840bf3bfad44b0d84718c86
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-07 23:19:38 +0800

    Update init_env.sh

--- init/init_env.sh
+++ init/init_env.sh
@@ -8,11 +8,11 @@ hostname=$2
 
 echo "===================更新软件包,安装依赖========================="
 if [[ $(cat /etc/os-release) == *"Ubuntu"* ]]; then
-          ##echo $pd |sudo -S apt install net-tools wget curl vim
+          ##echo $pd |sudo -S apt install net-tools wget curl vim expect
           echo $pd |sudo -S apt-get remove openjdk*
 elif [[ $(cat /etc/os-release) == *"CentOS"* ]]; then
-     echo $pd |sudo -S yum install -y net-tools wget curl vim expect
-          ##echo $pd |sudo -S rpm -e  --nodeps `rpm -qa |grep openjdk`
+     ## echo $pd |sudo -S yum install -y net-tools wget curl vim expect
+          echo $pd |sudo -S rpm -e  --nodeps `rpm -qa |grep openjdk`
           echo $pd |sudo -S systemctl stop firewalld.service
           echo $pd |sudo -S systemctl disable firewalld.service
 else
@@ -20,25 +20,21 @@ else
   echo "======================无法判断当前系统==================="
   printf "\n"
 fi
-echo "==================免密设置==========================="
-if [ -d ~/.ssh ]; then
-     echo "已经设置过ssh免密"
 
+if [ -d ~/.ssh ]; then
+     echo "==========已经设置过ssh免密==========="
 else
     ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa && cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys && chmod 0600 ~/.ssh/authorized_keys
-    echo "==========================ssh 设置成功========================================"
+
 fi
 
-echo "===================配置ip映射================================"
+printf "\n"
 
 ip=`ifconfig ens33|grep "inet " |awk '{ print $2}'`
 echo $pd |sudo -S hostnamectl set-hostname "$hostname"
 echo $pd | sudo -S sh -c "echo '${ip} $hostname' >> /etc/hosts"
 
+printf "\n"
 
-echo "=====================创建目录============================="
 echo $pd |sudo -S mkdir -p /opt/install
-
-if [ "$(uname)" != "Darwin" ]; then
-  echo $pd |sudo -S chown -R $USER:$USER /opt/install
-fi
+echo $pd |sudo -S chown -R $USER:$USER /opt/install
