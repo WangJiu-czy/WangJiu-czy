8c4227797cc29e92533a5d0a953bd7334e87410e
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-07 10:44:45 +0800

    Update init_env.sh

--- init/init_env.sh
+++ init/init_env.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 
-pd="hadoop"
+pd=$1
 proxy="https://ghproxy.com/"
-hostname="master"
+hostname=$2
 
 
 echo "===================更新软件包,安装依赖========================="
 if [[ $(cat /etc/os-release) == *"Ubuntu"* ]]; then
-          echo $pd |sudo -S apt install net-tools wget curl vim
+          ##echo $pd |sudo -S apt install net-tools wget curl vim
           echo $pd |sudo -S apt-get remove openjdk*
 elif [[ $(cat /etc/os-release) == *"CentOS"* ]]; then
      echo $pd |sudo -S yum install -y net-tools wget curl vim expect
-          echo $pd |sudo -S rpm -e  --nodeps `rpm -qa |grep openjdk`
+          ##echo $pd |sudo -S rpm -e  --nodeps `rpm -qa |grep openjdk`
           echo $pd |sudo -S systemctl stop firewalld.service
           echo $pd |sudo -S systemctl disable firewalld.service
 else
@@ -26,7 +26,7 @@ if [ -d ~/.ssh ]; then
 
 else
     ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa && cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys && chmod 0600 ~/.ssh/authorized_keys
-    echo "==========================ssh 设置成功!============================================"
+    echo "==========================ssh 设置成功========================================"
 fi
 
 echo "===================配置ip映射================================"
