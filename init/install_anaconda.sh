fe422c86d611835e0b060f0218869183d130632f
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-08 18:23:56 +0800

    Update install_anaconda.sh

--- init/install_anaconda.sh
+++ init/install_anaconda.sh
@@ -3,7 +3,7 @@
 set timeout 60
 set conda_path /opt/install/anaconda3
 
-spawn curl -fSLK /opt/install/ https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2020.11-Linux-x86_64.sh -o /opt/install/Anaconda.sh
+spawn curl -fSLK /opt/install/ https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2019.07-Linux-x86_64.sh -o /opt/install/Anaconda.sh
 expect  eof
 
 spawn bash /opt/install/Anaconda.sh -b -p $conda_path
