ffe5c8c6b90db736aff283c535643cb4425b825c
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-13 13:32:59 +0800

    Update install-spark.sh

--- init/install-spark.sh
+++ init/install-spark.sh
@@ -6,9 +6,9 @@ F3=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/co
 
 if [ -d /opt/install/spark/conf  ]; then
 
-wget -P /opt/install/spark/conf  $F1 -O /opt/install/hbase/conf/spark-defaults.conf
-wget -P /opt/install/spark/conf  $F2 -O /opt/install/hbase/conf/spark-env.sh
-wget -P /opt/install/spark/conf  $F3 -O /opt/install/hbase/conf/workers
+wget -P /opt/install/spark/conf  $F1 -O /opt/install/spark/conf/spark-defaults.conf
+wget -P /opt/install/spark/conf  $F2 -O /opt/install/spark/conf/spark-env.sh
+wget -P /opt/install/spark/conf  $F3 -O /opt/install/spark/conf/workers
 
 
 fi
