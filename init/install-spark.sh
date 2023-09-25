570bcf73eea007e87e1ad5224cda5c861cda4633
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-25 15:13:03 +0800

    Update install-spark.sh

--- init/install-spark.sh
+++ init/install-spark.sh
@@ -1,14 +1,26 @@
 proxy="https://ghproxy.com/"
 F1=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/conf/spark/spark-defaults.conf"
 F2=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/conf/spark/spark-env.sh"
-
 F3=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/conf/spark/workers"
+prePATH=$1
+SP_HOME=$prePATH"/spark"
+hostName=`hostname`
 
-if [ -d /opt/install/spark/conf  ]; then
+if [ -d $SP_HOME/conf  ]; then
+source /etc/profile && echo "=======加载 /etc/profile环境变量======"
+printf "\n"
+source ~/.bashrc && echo "=======加载 ~/.bashrc环境变量======"
+printf "\n"
+wget -P $SP_HOME/conf  $F1 -O $SP_HOME/conf/spark-defaults.conf
+wget -P $SP_HOME/conf  $F2 -O $SP_HOME/conf/spark-env.sh
+wget -P $SP_HOME/conf  $F3 -O $SP_HOME/conf/workers
+chmod +x  $SP_HOME/conf/spark-env.sh
 
-wget -P /opt/install/spark/conf  $F1 -O /opt/install/spark/conf/spark-defaults.conf
-wget -P /opt/install/spark/conf  $F2 -O /opt/install/spark/conf/spark-env.sh
-wget -P /opt/install/spark/conf  $F3 -O /opt/install/spark/conf/workers
+sed -i "s,master,$hostName,g" $SP_HOME/conf/spark-env.sh
+sed -i "s,/opt/install/java,$JAVA_HOME,g" $SP_HOME/conf/spark-env.sh
 
+if [ -d $HADOOP_HOME ]; then
+  echo "export HADOOP_CONF_DIR=/opt/install/hadoop/etc/hadoop" >> $SP_HOME/conf/spark-env.sh
+fi
 
 fi
