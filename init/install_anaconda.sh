71c55edd15607f3bf1c8eabf67e63384be52d211
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-25 13:49:03 +0800

    Update install_anaconda.sh

--- init/install_anaconda.sh
+++ init/install_anaconda.sh
@@ -2,10 +2,8 @@
 
 set timeout 60
 set conda_path /opt/install/anaconda3
-set URL https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2019.07-Linux-x86_64.sh
 
-
-spawn wget --no-check-certificate -P /opt/install $URL -O /opt/install/Anaconda.sh
+spawn curl -fSLK /opt/install/ https://mirrors.aliyun.com/anaconda/archive/Anaconda3-2020.02-Linux-x86_64.sh -o /opt/install/Anaconda.sh
 expect  eof
 
 spawn bash /opt/install/Anaconda.sh -b -p $conda_path
