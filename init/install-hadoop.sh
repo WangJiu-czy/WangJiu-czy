64b46fd51e7f28c0fc29e965ad4991b903d67aaa
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-25 14:39:45 +0800

    Update install-hadoop.sh

--- init/install-hadoop.sh
+++ init/install-hadoop.sh
@@ -1,4 +1,7 @@
 proxy="https://ghproxy.com/"
+prePATH=$1
+hostName=`hostname`
+HD_HOME=$prePath"/hadoop"
 HDENV=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/qx/shell/hadoop-env.sh"
 CORE=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/qx/shell/core-site.xml"
 HDFS=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/qx/shell/hdfs-site.xml"
@@ -6,13 +9,28 @@ MP=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/qx/shel
 YARN=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/qx/shell/yarn-site.xml"
 SLAVE=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/qx/shell/workers"
 
-if [ -d /opt/install/hadoop/etc/hadoop ]; then
+if [ -d $HD_HOME/etc/hadoop ]; then
+source /etc/profile && echo "=======加载 /etc/profile环境变量======"
+printf "\n"
+source ~/.bashrc && echo "=======加载 ~/.bashrc环境变量======"
+printf "\n"
+wget -P $HD_HOME/etc/hadoop $HDENV -O $HD_HOME/etc/hadoop/hadoop-env.sh
+wget -P $HD_HOME/etc/hadoop $CORE -O $HD_HOME/etc/hadoop/core-site.xml
+wget -P $HD_HOME/etc/hadoop $HDFS -O $HD_HOME/etc/hadoop/hdfs-site.xml
+wget -P $HD_HOME/etc/hadoop $MP  -O $HD_HOME/etc/hadoop/mapred-site.xml
+wget -P $HD_HOME/etc/hadoop $YARN -O $HD_HOME/etc/hadoop/yarn-site.xml
+###wget -P $HD_HOME/etc/hadoop $SLAVE -O  $HD_HOME/etc/hadoop/workers
 
-wget -P /opt/install/hadoop/etc/hadoop $HDENV -O /opt/install/hadoop/etc/hadoop/hadoop-env.sh
-wget -P /opt/install/hadoop/etc/hadoop $CORE -O /opt/install/hadoop/etc/hadoop/core-site.xml
-wget -P /opt/install/hadoop/etc/hadoop $HDFS -O /opt/install/hadoop/etc/hadoop/hdfs-site.xml
-wget -P /opt/install/hadoop/etc/hadoop $MP  -O /opt/install/hadoop/etc/hadoop/mapred-site.xml
-wget -P /opt/install/hadoop/etc/hadoop $YARN -O /opt/install/hadoop/etc/hadoop/yarn-site.xml
-wget -P /opt/install/hadoop/etc/hadoop $SLAVE -O  /opt/install/hadoop/etc/hadoop/workers
+sed -i 's,/opt/install/java,$JAVA_HOME,g' $HD_HOME/etc/hadoop/hadoop-env.sh
+sed -i 's,/opt/install/hadoop,$HADOOP_HOME,g' $HD_HOME/etc/hadoop/hadoop-env.sh
+sed -i 's,/opt/install/bigdata,$prePATH,g' $HD_HOME/etc/hadoop/core-site.xml
+sed -i 's,/opt/install/bigdata,$prePATH,g' $HD_HOME/etc/hadoop/hdfs-site.xml
+
+echo "======================================================================="
+sed -i 's,master,$hostName,g' $HD_HOME/etc/hadoop/core-site.xml
+sed -i 's,master,$hostName,g' $HD_HOME/etc/hadoop/hdfs-site.xml
+sed -i 's,master,$hostName,g' $HD_HOME/etc/hadoop/mapred-site.xml
+sed -i 's,master,$hostName,g' $HD_HOME/etc/hadoop/yarn-site.xml
+
 
 fi
