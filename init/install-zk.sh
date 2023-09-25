39fa318389bb16161393a7fd1e8ba9cb3cbc411d
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-25 15:34:03 +0800

    Update install-zk.sh

--- init/install-zk.sh
+++ init/install-zk.sh
@@ -1,4 +1,15 @@
 
-mkdir -p /opt/install/zookeeper/data && echo "1" > /opt/install/zookeeper/data/myid
-mv /opt/install/zookeeper/conf/zoo_sample.cfg  /opt/install/zookeeper/conf/zoo.cfg
-sed -i 's,/tmp/zookeeper,/opt/install/zookeeper/data,g' /opt/install/zookeeper/conf/zoo.cfg
+
+source /etc/profile && echo "=======加载 /etc/profile环境变量======"
+printf "\n"
+source ~/.bashrc && echo "=======加载 ~/.bashrc环境变量======"
+printf "\n"
+
+if [ -d $ZOOKEEPER_HOME ]; then
+
+mkdir -p $ZOOKEEPER_HOME/data && echo "1" > $ZOOKEEPER_HOME/data/myid
+mv $ZOOKEEPER_HOME/conf/zoo_sample.cfg  $ZOOKEEPER_HOME/conf/zoo.cfg
+sed -i "s,/tmp/zookeeper,$ZOOKEEPER_HOME/data,g" $ZOOKEEPER_HOME/conf/zoo.cfg
+
+
+fi
