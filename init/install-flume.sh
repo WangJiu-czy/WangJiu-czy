b4d8df7351313a4c9fb2041337dd4662deb45f9f
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-25 15:39:16 +0800

    Update install-flume.sh

--- init/install-flume.sh
+++ init/install-flume.sh
@@ -1,14 +1,20 @@
-if [ -d /opt/install/flume/conf ]; then
+source /etc/profile && echo "=======加载 /etc/profile环境变量======"
+printf "\n"
+source ~/.bashrc && echo "=======加载 ~/.bashrc环境变量======"
+printf "\n"
+
+
+if [ -d $FLUME_HOME/conf ]; then
 
 proxy="https://ghproxy.com/"
 FLENV=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/conf/flume/flume-env.sh"
 TSHDFS=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/conf/flume/hdfs-test.conf"
 TS2=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/conf/flume/test-flmue.sh"
 
-wget -P /opt/install/flume/conf $FLENV -O /opt/install/flume/conf/flume-env.sh
-wget -P /opt/install/flume/conf $TSHDFS -O /opt/install/flume/conf/hdfs-test.conf
-wget -P /opt/install/flume/conf $TS2 -O /opt/install/flume/conf/test-flmue.sh
+wget -P $FLUME_HOME/conf $FLENV -O $FLUME_HOME/conf/flume-env.sh
+wget -P $FLUME_HOME/conf $TSHDFS -O $FLUME_HOME/conf/hdfs-test.conf
+wget -P $FLUME_HOME/conf $TS2 -O $FLUME_HOME/conf/test-flmue.sh
 
-
+sed -i "s,/opt/install/java,$JAVA_HOME,g" FLUME_HOME/conf/flume-env.sh
 
 fi
