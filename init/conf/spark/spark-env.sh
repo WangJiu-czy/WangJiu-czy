8ab39be2f1a6d434b61cb591200893a06e49e576
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-25 15:12:33 +0800

    Update spark-env.sh

--- init/conf/spark/spark-env.sh
+++ init/conf/spark/spark-env.sh
@@ -7,9 +7,9 @@ export SPARK_WORKER_MEMOBY=1g
 export SPARK_WORKER_PORT=7078
 export SPARK_WORKER_WEBUI_PORT=7080
 
-export HADOOP_CONF_DIR=/opt/install/hadoop/etc/hadoop
+#export HADOOP_CONF_DIR=/opt/install/hadoop/etc/hadoop
 #配置指定的python解释器,这个是使用anaconda创建的虚拟环境
-export PYSPARK_PYTHON=/opt/install/anaconda3/bin/python3
-
-export PYSPARK_DRIVER_PYTHON=/opt/install/anaconda3/bin/python3
+export OPT=/opt/install
+export PYSPARK_PYTHON=$OPT/anaconda3/bin/python3
+export PYSPARK_DRIVER_PYTHON=$OPT/anaconda3/bin/python3
 #export SPARK_HISTORY_OPTS="-Dspark.history.fs.logDirectory=hdfs://master:8020/sparklog/ -Dspark.history.fs.cleaner.enable=true"
