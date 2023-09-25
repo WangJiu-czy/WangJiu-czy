abab834522d2eba543a8cfbfe75899e7d83fdc82
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-25 15:41:44 +0800

    Update install-hive.sh

--- init/install-hive.sh
+++ init/install-hive.sh
@@ -1,6 +1,17 @@
-path=$1
+
+source /etc/profile && echo "=======加载 /etc/profile环境变量======"
+printf "\n"
+source ~/.bashrc && echo "=======加载 ~/.bashrc环境变量======"
+printf "\n"
+
+if [ -d $HIVE_HOME/conf ]; then
+
+hostName=`hostname`
 HIVE_SITE="https://ghproxy.com/https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/qx/shell/hive-site.xml"
-wget -P $path/conf $HIVE_SITE -O $path/conf/hive-site.xml
-echo "nohup hive --service metastore 2>&1 &" >> $path/bin/hive-meta.sh
-echo "nohup hive --service hiveserver2 2>&1 &" >> $path/bin/hive2.sh
-chmod +x $path/bin/*.sh
+wget -P $HIVE_HOME/conf $HIVE_SITE -O $HIVE_HOME/conf/hive-site.xml
+echo "nohup hive --service metastore 2>&1 &" >> $HIVE_HOME/bin/hive-meta.sh
+echo "nohup hive --service hiveserver2 2>&1 &" >> $HIVE_HOME/bin/hive2.sh
+chmod +x $HIVE_HOME/bin/*.sh
+
+sed -i "s,master,$hostName,g" $HIVE_HOME/conf/hive-site.xml
+fi
