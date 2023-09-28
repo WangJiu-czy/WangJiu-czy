f849076d332e2edc633894820e47cafd6fe28d7c
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-28 14:26:41 +0800

    Update install-flume.sh

--- init/install-flume.sh
+++ init/install-flume.sh
@@ -11,9 +11,9 @@ FLENV=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init
 TSHDFS=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/conf/flume/hdfs-test.conf"
 TS2=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/conf/flume/test-flmue.sh"
 
-wget -P $FLUME_HOME/conf $FLENV -O $FLUME_HOME/conf/flume-env.sh
-wget -P $FLUME_HOME/conf $TSHDFS -O $FLUME_HOME/conf/hdfs-test.conf
-wget -P $FLUME_HOME/conf $TS2 -O $FLUME_HOME/conf/test-flmue.sh
+wget -q -t 3 -P $FLUME_HOME/conf $FLENV -O $FLUME_HOME/conf/flume-env.sh
+wget -q -t 3 -P $FLUME_HOME/conf $TSHDFS -O $FLUME_HOME/conf/hdfs-test.conf
+wget -q -t 3 -P $FLUME_HOME/conf $TS2 -O $FLUME_HOME/conf/test-flmue.sh
 
 sed -i "s,/opt/install/java,$JAVA_HOME,g" FLUME_HOME/conf/flume-env.sh
 chmod +x FLUME_HOME/conf/flume-env.sh
