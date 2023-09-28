f259d7479e330ac1411cc81a5ef72ba318d39d4f
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-28 11:10:03 +0800

    Update hadoop.sh

--- init/auto/hadoop.sh
+++ init/auto/hadoop.sh
@@ -13,7 +13,7 @@ cd /opt/install
 echo $pd |sudo -S sh -c "echo 'export OPT=/opt/install' >> /etc/profile"
 if [ ! -d /opt/install/java ]; then
      echo "===========================下载jdk=============================="
-     wget -P /opt/install  https://repo.huaweicloud.com/java/jdk/8u202-b08/jdk-8u202-linux-x64.tar.gz && tar -zxvf /opt/install/jdk-8u202-linux-x64.tar.gz -C /opt/install
+     wget -q --show-progress -P /opt/install  https://repo.huaweicloud.com/java/jdk/8u202-b08/jdk-8u202-linux-x64.tar.gz --no-check-certificate && tar -zxvf /opt/install/jdk-8u202-linux-x64.tar.gz -C /opt/install
      mv /opt/install/jdk1.8.0_202  /opt/install/java
      echo $pd |sudo -S sh -c "echo 'export JAVA_HOME=\$OPT/java' >> /etc/profile"
      echo $pd |sudo -S sh -c "echo 'export PATH=\$PATH:\$JAVA_HOME/bin' >> /etc/profile"
@@ -25,7 +25,7 @@ fi
 export HADOOP_VERSION=$3
 export  HADOOP_URL=https://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz
 
-curl -fSL "$HADOOP_URL" -o /opt/install/hadoop.tar.gz \
+curl -fSLk "$HADOOP_URL" -o /opt/install/hadoop.tar.gz \
     && tar -xvf /opt/install/hadoop.tar.gz -C /opt/install
 
 mv /opt/install/hadoop-$HADOOP_VERSION /opt/install/hadoop
