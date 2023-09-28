48656c7698cb78375df1a17d1abcad11474877cf
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-28 10:53:46 +0800

    Update install-zk.sh

--- init/install-zk.sh
+++ init/install-zk.sh
@@ -7,7 +7,7 @@ printf "\n"
 
 if [ -d $ZOOKEEPER_HOME ]; then
 
-mkdir -p $ZOOKEEPER_HOME/data && echo "1" > $ZOOKEEPER_HOME/data/myid
+mkdir -q -P $ZOOKEEPER_HOME/data && echo "1" > $ZOOKEEPER_HOME/data/myid
 mv $ZOOKEEPER_HOME/conf/zoo_sample.cfg  $ZOOKEEPER_HOME/conf/zoo.cfg
 sed -i "s,/tmp/zookeeper,$ZOOKEEPER_HOME/data,g" $ZOOKEEPER_HOME/conf/zoo.cfg
 
