a7a456e5a0a0e6718f5dbcf28f5b9fe1f02da801
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-28 11:02:27 +0800

    Update hadoop.sh

--- init/auto/hadoop.sh
+++ init/auto/hadoop.sh
@@ -5,50 +5,17 @@ proxy="https://ghproxy.com/"
 hostname=$2
 
 
-echo "===================更新软件包,安装依赖========================="
-# 检测当前系统是Ubuntu还是CentOS
-if [[ $(cat /etc/os-release) == *"Ubuntu"* ]]; then
-          ##echo $pd |sudo -S apt-get remove openjdk*
-elif [[ $(cat /etc/os-release) == *"CentOS"* ]]; then
-          echo $pd |sudo -S rpm -e  --nodeps `rpm -qa |grep openjdk`
-          echo $pd |sudo -S systemctl stop firewalld.service
-          echo $pd |sudo -S systemctl disable firewalld.service
-else
-  printf "\n"
-  echo "======================无法判断当前系统==================="
-  printf "\n"
-fi
+wget -qO - 'https://ghproxy.com/https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/init_env.sh' | bash -s "$pd" "$hostname"
 
 
-echo "==================免密设置==========================="
-if [ -d ~/.ssh ]; then
-      printf "\n"
-
-else
-    ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa && cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys && chmod 0600 ~/.ssh/authorized_keys
-
-fi
-
-echo "===================配置ip映射================================"
-printf "\n"
-ip=`ifconfig ens33|grep "inet " |awk '{ print $2}'`
-echo $pd |sudo -S hostnamectl set-hostname "$hostname"
-echo $pd | sudo -S sh -c "echo '${ip} $hostname' >> /etc/hosts"
-
-
-echo $pd |sudo -S mkdir -p /opt/install
-echo $pd |sudo -S chown -R $USER:$USER /opt/install
-
-# shellcheck disable=SC2164
 cd /opt/install
 
-#------------------------------mac从这开始--------------------------------------------
-
+echo $pd |sudo -S sh -c "echo 'export OPT=/opt/install' >> /etc/profile"
 if [ ! -d /opt/install/java ]; then
      echo "===========================下载jdk=============================="
      wget -P /opt/install  https://repo.huaweicloud.com/java/jdk/8u202-b08/jdk-8u202-linux-x64.tar.gz && tar -zxvf /opt/install/jdk-8u202-linux-x64.tar.gz -C /opt/install
      mv /opt/install/jdk1.8.0_202  /opt/install/java
-     echo $pd |sudo -S sh -c "echo 'export JAVA_HOME=/opt/install/java' >> /etc/profile"
+     echo $pd |sudo -S sh -c "echo 'export JAVA_HOME=\$OPT/java' >> /etc/profile"
      echo $pd |sudo -S sh -c "echo 'export PATH=\$PATH:\$JAVA_HOME/bin' >> /etc/profile"
      printf "\n"
 fi
@@ -76,7 +43,7 @@ printf "\n"
 
 echo "=================设置环境变量======================================"
 
-echo $pd |sudo -S sh -c "echo 'export OPT=/opt/install' >> /etc/profile"
+printf "\n"
 echo $pd |sudo -S sh -c "echo 'export HADOOP_HOME=\$OPT/hadoop' >> /etc/profile"
 echo $pd |sudo -S sh -c "echo 'export PATH=\$PATH:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin' >> /etc/profile"
 echo $pd |sudo -S sh -c "echo 'export HADOOP_CONF_DIR=\$HADOOP_HOME/etc/hadoop' >> /etc/profile"
