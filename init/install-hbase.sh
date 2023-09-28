bd2a5c4088b48030fae72319185f7e275981d8b3
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-28 10:39:30 +0800

    Update install-hbase.sh

--- init/install-hbase.sh
+++ init/install-hbase.sh
@@ -3,18 +3,17 @@ HBENV=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init
 CORE=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/conf/hbase/hbase-site.xml"
 SLAVE=$proxy"https://github.com/WangJiu-czy/WangJiu-czy/blob/main/init/conf/hbase/regionservers"
 
-source /etc/profile && echo "=======加载 /etc/profile环境变量======"
+source /etc/profile 
 printf "\n"
-source ~/.bashrc && echo "=======加载 ~/.bashrc环境变量======"
 printf "\n"
 
 
 hostName=`hostname`
 if [ -d $HBASE_HOME  ]; then
 
-wget -P $HBASE_HOME/conf $HBENV -O $HBASE_HOME/conf/hbase-env.sh
-wget -P $HBASE_HOME/conf $CORE -O $HBASE_HOME/conf/hbase-site.xml
-wget -P $HBASE_HOME/conf $SLAVE -O $HBASE_HOME/conf/regionservers
+wget -q -P $HBASE_HOME/conf $HBENV -O $HBASE_HOME/conf/hbase-env.sh
+wget -q -P $HBASE_HOME/conf $CORE -O $HBASE_HOME/conf/hbase-site.xml
+wget -q -P $HBASE_HOME/conf $SLAVE -O $HBASE_HOME/conf/regionservers
 
 sed -i "s,master,$hostName,g"  $HBASE_HOME/conf/regionservers
 sed -i "s,/opt/install/java,$JAVA_HOME,g" $HBASE_HOME/conf/hbase-env.sh
