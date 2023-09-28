55317e59a7bdab0aae1e4cdcade57b8c2495b47b
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-09-28 14:26:19 +0800

    Update install-hive.sh

--- init/install-hive.sh
+++ init/install-hive.sh
@@ -8,7 +8,7 @@ if [ -d $HIVE_HOME/conf ]; then
 
 hostName=`hostname`
 HIVE_SITE="https://ghproxy.com/https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/qx/shell/hive-site.xml"
-wget -q --show-progress -P $HIVE_HOME/conf $HIVE_SITE -O $HIVE_HOME/conf/hive-site.xml
+wget -q -t 3  -P $HIVE_HOME/conf $HIVE_SITE -O $HIVE_HOME/conf/hive-site.xml
 echo "nohup hive --service metastore 2>&1 &" >> $HIVE_HOME/bin/hive-meta.sh
 echo "nohup hive --service hiveserver2 2>&1 &" >> $HIVE_HOME/bin/hive2.sh
 chmod +x $HIVE_HOME/bin/*.sh
