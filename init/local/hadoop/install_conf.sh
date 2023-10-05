096cc42b8ead8921df9fe119570dcab5bc3bedde
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-10-05 18:33:13 +0800

    Update install_conf.sh

--- init/local/hadoop/install_conf.sh
+++ init/local/hadoop/install_conf.sh
@@ -1,6 +1,6 @@
 proxy="https://ghproxy.com/"
 
-HDENV=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/local/hadoop/hadoop-env.sh"
+HDENV=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/qx/shell/hadoop-env.sh"
 CORE=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/local/hadoop/core-site.xml"
 HDFS=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/local/hadoop/hdfs-site.xml"
 MP=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/local/hadoop/mapred-site.xml"
@@ -18,5 +18,15 @@ wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $CORE -O $HADOOP_HOME/etc/hadoop/core-s
 wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $HDFS -O $HADOOP_HOME/etc/hadoop/hdfs-site.xml
 wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $MP  -O $HADOOP_HOME/etc/hadoop/mapred-site.xml
 wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $YARN -O $HADOOP_HOME/etc/hadoop/yarn-site.xml
+
+
+sed -i "s,/opt/install/java,$JAVA_HOME,g" $HADOOP_HOME/etc/hadoop/hadoop-env.sh
+sed -i "s,/opt/install/hadoop,$HADOOP_HOME,g" $HADOOP_HOME/etc/hadoop/hadoop-env.sh
+sed -i "s,/usr/local/hadoop,$HADOOP_HOME,g" $HADOOP_HOME/etc/hadoop/core-site.xml
+sed -i "s,/usr/local/hadoop,$HADOOP_HOME,g" $HADOOP_HOME/etc/hadoop/hdfs-site.xml
+
+value_path=`hadoop classpath`
+sed -i "s,my_hadoop_classpath,$value_path,g" $HADOOP_HOME/etc/hadoop/yarn-site.xml
+
 
 fi
