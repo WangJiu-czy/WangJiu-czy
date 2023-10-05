e32dd41c38aa800a653cfd4585699d787aebac03
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-10-05 18:26:22 +0800

    Update install_conf.sh

--- init/local/hadoop/install_conf.sh
+++ init/local/hadoop/install_conf.sh
@@ -1,20 +1,22 @@
 proxy="https://ghproxy.com/"
+
 HDENV=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/local/hadoop/hadoop-env.sh"
 CORE=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/local/hadoop/core-site.xml"
 HDFS=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/local/hadoop/hdfs-site.xml"
 MP=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/local/hadoop/mapred-site.xml"
 YARN=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/local/hadoop/yarn-site.xml"
 
+source /etc/profile && echo "=======加载 /etc/profile环境变量======"
+printf "\n"
+source ~/.bashrc && echo "=======加载 ~/.bashrc环境变量======"
+printf "\n"
 
-path="/usr/local/hadoop/etc/hadoop"
+if [ -d $HADOOP_HOME ]; then
 
-if [ -d $path ]; then
-
-wget -q -P $path $HDENV -O $path/hadoop-env.sh
-wget -q -P $path $CORE -O $path/core-site.xml
-wget -q -P $path $HDFS -O $path/hdfs-site.xml
-wget -q -P $path $MP  -O $path/mapred-site.xml
-wget -q -P $path $YARN -O $path/yarn-site.xml
-
+wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $HDENV -O $HADOOP_HOME/etc/hadoop/hadoop-env.sh
+wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $CORE -O $HADOOP_HOME/etc/hadoop/core-site.xml
+wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $HDFS -O $HADOOP_HOME/etc/hadoop/hdfs-site.xml
+wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $MP  -O $HADOOP_HOME/etc/hadoop/mapred-site.xml
+wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $YARN -O $HADOOP_HOME/etc/hadoop/yarn-site.xml
 
 fi
