ac4b47c0bc21c414510de573b16d5338d03fcdc8
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-10-11 11:44:43 +0800

    Update install-hbase.sh

--- init/install-hbase.sh
+++ init/install-hbase.sh
@@ -3,7 +3,7 @@ HBENV=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init
 CORE=$proxy"https://raw.githubusercontent.com/WangJiu-czy/WangJiu-czy/main/init/conf/hbase/hbase-site.xml"
 SLAVE=$proxy"https://github.com/WangJiu-czy/WangJiu-czy/blob/main/init/conf/hbase/regionservers"
 
-source /etc/profile 
+source /etc/profile  && echo "=============加载软件源==============="
 printf "\n"
 printf "\n"
 
@@ -24,3 +24,4 @@ sed -i "s,master,$hostName,g"  $HBASE_HOME/conf/regionservers
 sed -i "s,/opt/install/java,$JAVA_HOME,g" $HBASE_HOME/conf/hbase-env.sh
 sed -i "s,master,$hostName,g"  $HBASE_HOME/conf/hbase-site.xml
 fi
+echo "=============加载成功==============="
