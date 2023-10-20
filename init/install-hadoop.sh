0e5d5e12cbf1b0ce8b4cbed98685744d6fa0fd7b
小蔡不爱吃菜 <59927567+WangJiu-czy@users.noreply.github.com>
2023-10-20 13:54:50 +0800

    Update install-hadoop.sh

--- init/install-hadoop.sh
+++ init/install-hadoop.sh
@@ -20,24 +20,24 @@ printf "\n"
 
 HD_HOME=$HADOOP_HOME
 
-if [ -d $HD_HOME/etc/hadoop ]; then
+if [ -d $HADOOP_HOME/etc/hadoop ]; then
 
-wget -q -t 3  -P $HD_HOME/etc/hadoop $HDENV -O $HD_HOME/etc/hadoop/hadoop-env.sh --no-check-certificate
-wget -q -t 3  -P $HD_HOME/etc/hadoop $CORE -O $HD_HOME/etc/hadoop/core-site.xml --no-check-certificate
-wget -q -t 3  -P $HD_HOME/etc/hadoop $HDFS -O $HD_HOME/etc/hadoop/hdfs-site.xml --no-check-certificate
-wget -q -t 3  -P $HD_HOME/etc/hadoop $MP  -O $HD_HOME/etc/hadoop/mapred-site.xml --no-check-certificate
-wget -q -t 3  -P $HD_HOME/etc/hadoop $YARN -O $HD_HOME/etc/hadoop/yarn-site.xml --no-check-certificate
+wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $HDENV -O $HADOOP_HOME/etc/hadoop/hadoop-env.sh --no-check-certificate
+wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $CORE -O $HADOOP_HOME/etc/hadoop/core-site.xml --no-check-certificate
+wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $HDFS -O $HADOOP_HOME/etc/hadoop/hdfs-site.xml --no-check-certificate
+wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $MP  -O $HADOOP_HOME/etc/hadoop/mapred-site.xml --no-check-certificate
+wget -q -t 3  -P $HADOOP_HOME/etc/hadoop $YARN -O $HADOOP_HOME/etc/hadoop/yarn-site.xml --no-check-certificate
 
 
-sed -i "s,/opt/install/java,$JAVA_HOME,g" $HD_HOME/etc/hadoop/hadoop-env.sh
-sed -i "s,/opt/install/hadoop,$HADOOP_HOME,g" $HD_HOME/etc/hadoop/hadoop-env.sh
-sed -i "s,/opt/install/bigdata,$HADOOP_HOME,g" $HD_HOME/etc/hadoop/core-site.xml
-sed -i "s,/opt/install/bigdata,$HADOOP_HOME,g" $HD_HOME/etc/hadoop/hdfs-site.xml
+sed -i "s,/opt/install/java,$JAVA_HOME,g" $HADOOP_HOME/etc/hadoop/hadoop-env.sh
+sed -i "s,/opt/install/hadoop,$HADOOP_HOME,g" $HADOOP_HOME/etc/hadoop/hadoop-env.sh
+sed -i "s,/opt/install/bigdata,$HADOOP_HOME,g" $HADOOP_HOME/etc/hadoop/core-site.xml
+sed -i "s,/opt/install/bigdata,$HADOOP_HOME,g" $HADOOP_HOME/etc/hadoop/hdfs-site.xml
 printf "\n"
-sed -i "s,master,$hostName,g" $HD_HOME/etc/hadoop/core-site.xml
-sed -i "s,master,$hostName,g" $HD_HOME/etc/hadoop/hdfs-site.xml
-sed -i "s,master,$hostName,g" $HD_HOME/etc/hadoop/mapred-site.xml
-sed -i "s,master,$hostName,g" $HD_HOME/etc/hadoop/yarn-site.xml
+sed -i "s,master,$hostName,g" $HADOOP_HOME/etc/hadoop/core-site.xml
+sed -i "s,master,$hostName,g" $HADOOP_HOME/etc/hadoop/hdfs-site.xml
+sed -i "s,master,$hostName,g" $HADOOP_HOME/etc/hadoop/mapred-site.xml
+sed -i "s,master,$hostName,g" $HADOOP_HOME/etc/hadoop/yarn-site.xml
 value_path=`hadoop classpath`
 sed -i "s,my_hadoop_classpath,$value_path,g" $HADOOP_HOME/etc/hadoop/yarn-site.xml
 echo "=======更新完成======="
